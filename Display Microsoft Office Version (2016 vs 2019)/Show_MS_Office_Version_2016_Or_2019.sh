68e9acd74be71b2d2a3606dc545e1909d22ecc76
Ventura Torres <34493084+VnKT26@users.noreply.github.com>
2021-12-09 19:20:10 -0700

    Update Show_MS_Office_Version_2016_Or_2019.sh

--- Display Microsoft Office Version (2016 vs 2019)/Show_MS_Office_Version_2016_Or_2019.sh
+++ Display Microsoft Office Version (2016 vs 2019)/Show_MS_Office_Version_2016_Or_2019.sh
@@ -20,7 +20,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-#Checks for if Client is running  Microsoft Office 2016 or Microsoft Office 2019 based on the presence of Excel
+#Checks for if Client is running  Microsoft Office 2016 or Microsoft Office 2019.
 Check2016(){
 
 wordversion=$(defaults read /Applications/Microsoft\ Word.app/Contents/Info.plist CFBundleVersion)
